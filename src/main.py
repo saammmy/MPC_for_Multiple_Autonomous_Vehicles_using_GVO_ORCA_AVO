a49df16fd0ddd91d6713f1ddbc557ca0d376ac66
saammmy <saammmy@github.com>
2022-04-18 15:59:39 -0400

    Added two vehicles, updated plot and example.txt

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from cmath import cos
 from turtle import color
 import autograd.numpy as np
 from autograd import grad
@@ -7,7 +8,7 @@ from matplotlib import pyplot as plt
 
 class Vehicle:
 
-    def __init__(self, start, goal, max_v, max_phi, start_time):
+    def __init__(self, start, goal, inital_control_input, max_v, max_phi, start_time):
     
         '''
         Start, goal and state contains:
@@ -17,48 +18,57 @@ class Vehicle:
             delta : Steering Angle (radians)
         These are the states of the Vehicle
         '''
-        x=start[0]
-        y=start[1]
-        theta=start[2]
-        delta=start[3]
+        x_start=start[0]
+        y_start=start[1]
+        theta_start=start[2]*np.pi/180
+        delta_start=start[3]*np.pi/180
         
-        self.start = [x, y, theta, delta]
+        self.start = [x_start, y_start, theta_start, delta_start]
 
-        self.goal = goal
+        x_goal=goal[0]
+        y_goal=goal[1]
+        theta_goal=goal[2]*np.pi/180
+        delta_goal=goal[3]*np.pi/180
+        self.goal = [x_goal, y_goal, theta_goal, delta_goal]
 
-        self.state = [x, y, theta, delta]
-        self.virtual_state = [x, y, theta, delta]
+        self.state = [x_start, y_start, theta_start, delta_start]
+        self.virtual_state = [x_start, y_start, theta_start, delta_start]
         '''
         # Control Input = [v, phi]
         # v : Velocity (m/s)
         # phi : Steering Rate (rad/s) 
         '''
-        self.control_input = [0.0,0.0]
+        initial_v = inital_control_input[0]
+        inital_phi = inital_control_input[1]*np.pi/180
+        self.control_input = [initial_v, inital_phi]
 
         # Length of the Vehicle
         self.length = 0.1
         self.size = 1
 
         # Obstacles
-        self.offset = 2
-        self.obstacles = [[5,3,0.5],[5,7,0.5]]
+        self.offset = 7
+        self.dynamic_offset = 10
+        self.obstacles = [[0,10,0.5],[10,0,0.5],[10,20,0.5],[20,10,0.5],[10,10,0.5]]
+        self.dynamic_obstacles=[[5,0,0.25,90*np.pi/180,0.5],[0,5,0.25,0*np.pi/180,0.5],[0,15,0.25,0*np.pi/180,0.5],[15,20,0.4,-90*np.pi/180,0.5]]
 
-        # Start Time of the Robot
+        # self.dynamic_obstacles=[[18.5,21.5,0.5,230*np.pi/180,0.5],[21.5,18.5,0.5,220*np.pi/180,0.5]]
+        # Start Time of the Vehicle
         # self.start_time = start_time
         self.time = start_time
         self.prediction_horizon = 10
         self.control_horizon = 1.0
-        self.sampling_time = 0.25
+        self.sampling_time = 0.2
         self.max_v = max_v
         self.max_phi = max_phi
 
         # History of the Vehicle
         self.state_history = []
-        self.state_history.append([x, y, theta, delta])
+        self.state_history.append([x_start, y_start, theta_start, delta_start])
         self.time_history = []
         self.time_history.append(start_time)
         self.control_input_history = []
-        self.control_input_history.append([0.0,0.0])
+        self.control_input_history.append([initial_v, inital_phi])
 
     
     def bicycle_model(self,v,phi, update = False):
@@ -114,14 +124,13 @@ class Vehicle:
         # Weights
         W1 = 1
         W2 = 1
-        W3 = 1
+        W3 = 0
 
         goal_reach_cost = W1*cost_xy + W2*cost_theta + W3*cost_delta
 
         return goal_reach_cost
 
     
-
     def smoothness_cost(self, virtual_input):
         cost_v = ((self.control_input[0] - virtual_input[0])/self.sampling_time)
         cost_phi = ((self.control_input[1] - virtual_input[1])/self.sampling_time)
@@ -140,24 +149,45 @@ class Vehicle:
         lamda_ws = 0 #Both x and y workspace constraints
         
         lambda_safety = 0
-
         for i in range(len(self.obstacles)):
             obs_x = self.obstacles[i][0]
             obs_y = self.obstacles[i][1]
-            dist = (((self.virtual_state[0]-obs_x)**2 + (self.virtual_state[1]-obs_y)**2)**0.5)
+            # dist = (((self.virtual_state[0]-obs_x)**2 + (self.virtual_state[1]-obs_y)**2)**0.5)
+            dist = (((self.state[0]-obs_x)**2 + (self.state[1]-obs_y)**2)**0.5)
             if dist < self.offset:
-                lambda_safety += max(0,-dist + self.obstacles[i][2] + self.size/2)
+                dist_virtual = (((self.virtual_state[0]-obs_x)**2 + (self.virtual_state[1]-obs_y)**2)**0.5)
+                lambda_safety += max(0,-dist_virtual + self.obstacles[i][2] + self.size/2)
+
+        for i in range(len(self.dynamic_obstacles)):
+            obs_x = self.dynamic_obstacles[i][0]
+            obs_y = self.dynamic_obstacles[i][1]
+            dist = (((self.state[0]-obs_x)**2 + (self.state[1]-obs_y)**2)**0.5)
+            if dist < self.dynamic_offset:
+                dist_virtual = (((self.virtual_state[0]-obs_x)**2 + (self.virtual_state[1]-obs_y)**2)**0.5)
+                lambda_safety += max(0,-dist_virtual + self.dynamic_obstacles[i][4] + self.size/2)
                    
-            
         # Weights
         W1 = 1
         W2 = 1
         W3 = 1
-        W4 = 1
+        W4 = 5
 
         constraint_cost = W1*lamda_v + W2*lamda_phi + W3*lamda_ws + W4*lambda_safety
         
         return constraint_cost
+
+    def dynamic_obstacle_cost(self):
+        dynamic_obstacle_cost = 0
+        for i in range(len(self.dynamic_obstacles)):
+            obs_x = self.dynamic_obstacles[i][0]
+            obs_y = self.dynamic_obstacles[i][1]
+
+            dist = (((self.state[0]-obs_x)**2 + (self.state[1]-obs_y)**2)**0.5)
+            if dist < self.dynamic_offset:
+                dist_virtual = (((self.virtual_state[0]-obs_x)**2 + (self.virtual_state[1]-obs_y)**2)**0.5)
+                dynamic_obstacle_cost += 1/dist_virtual
+
+        return dynamic_obstacle_cost
 
     def static_obstacle_cost(self):
         static_obstacle_cost = 0
@@ -165,10 +195,10 @@ class Vehicle:
             obs_x = self.obstacles[i][0]
             obs_y = self.obstacles[i][1]
 
-            dist = (((self.virtual_state[0]-obs_x)**2 + (self.virtual_state[1]-obs_y)**2)**0.5)
-
+            dist = (((self.state[0]-obs_x)**2 + (self.state[1]-obs_y)**2)**0.5)
             if dist < self.offset:
-                static_obstacle_cost += 1/dist
+                dist_virtual = (((self.virtual_state[0]-obs_x)**2 + (self.virtual_state[1]-obs_y)**2)**0.5)
+                static_obstacle_cost += 1/dist_virtual               
     
         return static_obstacle_cost       
 
@@ -182,12 +212,17 @@ class Vehicle:
         self.virtual_state = [x,y,theta,delta]
         
         # Calculating cost across the prediction horizon
-        for _ in range(self.prediction_horizon):
+        for i in range(self.prediction_horizon):
             W1=1
             W2=1
-            W3=1
-            W4=100
-            total_cost += self.goal_reach_cost()*W1 + self.smoothness_cost(virtual_input)*W2 + self.constraint_cost(virtual_input)*W3 + self.static_obstacle_cost()*W4
+            W3=10
+            W4=1000
+            W5=1000
+            if i == self.prediction_horizon-1:
+                terminal_cost = self.goal_reach_cost()*W1*5
+            else:
+                terminal_cost = 0
+            total_cost += self.goal_reach_cost()*W1 + self.smoothness_cost(virtual_input)*W2 + self.constraint_cost(virtual_input)*W3 + self.static_obstacle_cost()*W4 + self.dynamic_obstacle_cost()*W5 +terminal_cost
             self.bicycle_model(virtual_input[0],virtual_input[1])
             # print("Me here")
         
@@ -204,50 +239,123 @@ class Vehicle:
             mean_square_gradient = (0.9*mean_square_gradient) + 0.1 * (cost_gradient**2)
             virtual_input = virtual_input - (learning_rate/((mean_square_gradient)**0.5+eps))*cost_gradient
         self.control_input = virtual_input
-                
+    
+
+    def vehicle_plot(self, board):
+        # if(self.time!=self.time_history[1]):
+        #     vehicle.remove()
+        #     arrow.remove()
+
+        vehicle = plt.Circle((self.state[0], self.state[1]),0.5, facecolor='#3498db', edgecolor='black')
+        board.add_patch(vehicle)
+        arrow = plt.arrow(self.state[0], self.state[1], np.cos(self.state[2]), np.sin(self.state[2]), width = 0.1, facecolor='#3498db', edgecolor='black')
+        plt.plot(np.array(self.state_history)[:,0], np.array(self.state_history)[:,1], color='#3498db')
+
+        plt.plot(self.start[0],self.start[1], marker='*',  markersize=20, color='#1e8449', markeredgecolor='black')
+        plt.plot(self.goal[0],self.goal[1], marker='*',  markersize=20, color='#1e8449', markeredgecolor='black')
+
+        return vehicle, arrow
+    
+    def obstacle_plot(self, board):
+        # if(self.time!=self.time_history[1]):
+        #     dynamic_obstacle.remove()
+        dynamic_obstacles_plot=[]
+        for i in range(len(self.obstacles)):
+            obstacle = plt.Circle((self.obstacles[i][0], self.obstacles[i][1]),self.obstacles[i][2], facecolor='#c0392b', edgecolor='black')
+            board.add_patch(obstacle)
+        
+        for i in range(len(self.dynamic_obstacles)):
+            dynamic_obstacle = plt.Circle((self.dynamic_obstacles[i][0], self.dynamic_obstacles[i][1]),self.dynamic_obstacles[i][4], facecolor='#8e44ad', edgecolor='black')
+            board.add_patch(dynamic_obstacle)
+            dynamic_obstacles_plot.append(dynamic_obstacle)
+
+        return dynamic_obstacles_plot
+
+
 if __name__== '__main__':
 
-    Start_A = [0.0, 0.0, 0.0, 0.0]
-    Goal_A = [10.0, 10.0, np.pi/4, 0.0]
-    start_time = 0 #time()
-    VehicleA = Vehicle(Start_A,Goal_A, 25.0, 0.5,start_time)
-    # print(VehicleA.total_cost(Start_A))
-    fig = plt.figure(1)
-    board = plt.axes(xlim=(-5,15),ylim=(-5,12.5))
+    Start_A = [0.0, 0.0, 45.0, 0.0]
+    Goal_A = [20.0, 20.0, 45.0, 0.0]
 
-    while(((VehicleA.state[0] - VehicleA.goal[0])**2 > 1) or ((VehicleA.state[1] - VehicleA.goal[1])**2 > 1)):
+    Start_B = [20.0, 20.0, 225.0, 0.0]
+    Goal_B = [5.0, 5.0, 225.0, 0.0]
+
+
+    inital_control_input_A=[2.0,0.0]
+    inital_control_input_B=[2.0,0.0]
+    start_time = 0 #time()
+
+    VehicleA = Vehicle(Start_A,Goal_A, inital_control_input_A, 10.0, 10,start_time)
+
+    VehicleB = Vehicle(Start_B,Goal_B, inital_control_input_B, 10.0, 10,start_time)
+    
+    fig = plt.figure(1)
+    board = plt.axes(xlim=(-5,50),ylim=(-5,25))
+
+    while(((VehicleA.state[0] - VehicleA.goal[0])**2 > 2) or ((VehicleA.state[1] - VehicleA.goal[1])**2 > 2) or ((VehicleB.state[0] - VehicleB.goal[0])**2 > 2) or ((VehicleB.state[1] - VehicleB.goal[1])**2 > 2)):        
+
         VehicleA.optimizer()
         VehicleA.bicycle_model(VehicleA.control_input[0], VehicleA.control_input[1], True)
-        
-        for i in range(len(VehicleA.obstacles)):
-            obstacle = plt.Circle((VehicleA.obstacles[i][0], VehicleA.obstacles[i][1]),VehicleA.obstacles[i][2], color='orange')
-            board.add_patch(obstacle)
 
-        vehicle_a = plt.Circle((VehicleA.state[0], VehicleA.state[1]),0.5, color='blue')
-        board.add_patch(vehicle_a)
+        for i in range(len(VehicleA.dynamic_obstacles)):
+            VehicleA.dynamic_obstacles[i][0] += VehicleA.dynamic_obstacles[i][2]*np.cos(VehicleA.dynamic_obstacles[i][3])
+            VehicleA.dynamic_obstacles[i][1] += VehicleA.dynamic_obstacles[i][2]*np.sin(VehicleA.dynamic_obstacles[i][3])
+       
+        vehicle_a, arrow_a = VehicleA.vehicle_plot(board)
+        dynamic_obstacle =VehicleA.obstacle_plot(board)
+        # vehicle_a = plt.Circle((VehicleA.state[0], VehicleA.state[1]),0.5, facecolor='#3498db', edgecolor='black')
+        # board.add_patch(vehicle_a)
+        # arrowa = plt.arrow(VehicleA.state[0], VehicleA.state[1], np.cos(VehicleA.state[2]), np.sin(VehicleA.state[2]), width = 0.1, facecolor='#3498db', edgecolor='black')
+        # plt.plot(np.array(VehicleA.state_history)[:,0], np.array(VehicleA.state_history)[:,1], color='#3498db')
+
+        # plt.plot(VehicleA.start[0],VehicleA.start[1], marker='*',  markersize=20, color='#1e8449', markeredgecolor='black')
+        # plt.plot(VehicleA.goal[0],VehicleA.goal[1], marker='*',  markersize=20, color='#1e8449', markeredgecolor='black')
+
+        VehicleB.optimizer()
+        VehicleB.bicycle_model(VehicleB.control_input[0], VehicleB.control_input[1], True)
+        vehicle_b, arrow_b = VehicleB.vehicle_plot(board)
+
+        # vehicle_b = plt.Circle((VehicleB.state[0], VehicleB.state[1]),0.5, facecolor='#3498db', edgecolor='black')
+        # board.add_patch(vehicle_b)
+        # arrowb = plt.arrow(VehicleB.state[0], VehicleB.state[1], np.cos(VehicleB.state[2]), np.sin(VehicleB.state[2]), width = 0.1, facecolor='#3498db', edgecolor='black')
+        # plt.plot(np.array(VehicleB.state_history)[:,0], np.array(VehicleB.state_history)[:,1], color='#3498db')
+
+        # for i in range(len(VehicleA.obstacles)):
+        #     obstacle = plt.Circle((VehicleA.obstacles[i][0], VehicleA.obstacles[i][1]),VehicleA.obstacles[i][2], facecolor='#c0392b', edgecolor='black')
+        #     board.add_patch(obstacle)
         
+        # for i in range(len(VehicleA.dynamic_obstacles)):
+        #     dynamic_obstacle = plt.Circle((VehicleA.dynamic_obstacles[i][0], VehicleA.dynamic_obstacles[i][1]),VehicleA.dynamic_obstacles[i][4], facecolor='#8e44ad', edgecolor='black')
+        #     board.add_patch(dynamic_obstacle)
+              
         plt.draw()
         plt.pause(VehicleA.sampling_time*0.1)
+        
+        for i in dynamic_obstacle:
+            i.remove()
+
         vehicle_a.remove()
-        plt.scatter(VehicleA.state[0], VehicleA.state[1])
+        arrow_a.remove()
+        
+        vehicle_b.remove()
+        arrow_b.remove()
+        # vehicle_b.remove()
+        # arrowb.remove()
         # print("Time=",VehicleA.time)
        
 
     control_input_history = np.array(VehicleA.control_input_history)
     state_history = np.array(VehicleA.state_history)
     time_history = np.array(VehicleA.time_history)
-
-    # print(control_input_history)
-    # print(state_history)
-    # print(VehicleA.start)
-   
+    residual_state_history = np.array(VehicleA.state_history)-np.array(VehicleA.goal)
+           
     plt.figure(2)
     plt.title('Velocity')
     plt.plot(time_history,control_input_history[:,0])
     
     plt.figure(3)
-    plt.title('Stearing Rate')
-    plt.plot(time_history,control_input_history[:,1])
+    plt.title('Steering Rate (Degree/s)')
+    plt.plot(time_history,control_input_history[:,1]*180/np.pi)
 
     plt.figure(4)
     plt.title('X Position')
@@ -258,12 +366,24 @@ if __name__== '__main__':
     plt.plot(time_history,state_history[:,1])
 
     plt.figure(6)
-    plt.title('Theta')
-    plt.plot(time_history,state_history[:,2])
+    plt.title('Theta (Degree)')
+    plt.plot(time_history,state_history[:,2]*180/np.pi)
 
     plt.figure(7)
-    plt.title('Delta')
-    plt.plot(time_history,state_history[:,3])
+    plt.title('Delta (Degree)')
+    plt.plot(time_history,state_history[:,3]*180/np.pi)
+
+    plt.figure(8)
+    plt.title('XY Position Residual')
+    plt.plot(time_history,(residual_state_history[:,0]**2+residual_state_history[:,1]**2)**0.5)
+
+    plt.figure(9)
+    plt.title('Theta Residual(Degree)')
+    plt.plot(time_history,residual_state_history[:,2]*180/np.pi)
+
+    plt.figure(10)
+    plt.title('Delta Residual (Degree)')
+    plt.plot(time_history,residual_state_history[:,3]*180/np.pi)
 
     plt.show()
 
